dd3d4e449fdf75f044beae3418db473819a07716
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2023-05-20 20:08:54 +0300

    fix: replace numericupdown value type to decimal (responding to avalonia11 changes)

--- Converters/MathAddConverter.cs
+++ Converters/MathAddConverter.cs
@@ -8,11 +8,11 @@ public class MathAddConverter : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        return (double?)value + (double?)parameter;
+        return (decimal?)value + (decimal?)parameter;
     }
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        return (double?)value - (double?)parameter;
+        return (decimal?)value - (decimal?)parameter;
     }
 }
