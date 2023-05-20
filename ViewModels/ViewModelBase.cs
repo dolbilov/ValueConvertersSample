62c9e5b434ce747e62f9f1be46a0c9a7abc46d37
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2023-05-20 14:29:42 +0300

    Initial commit

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace ValueConvertersSample.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ValueConvertersSample.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
