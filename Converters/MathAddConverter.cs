621ab310af49215a0e76fe637399376aa4c2d0de
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2023-05-20 14:41:03 +0300

    chore: create MathAddConverter.cs

---
+++ Converters/MathAddConverter.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace ValueConvertersSample.Converters;
+
+public class MathAddConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return (double?)value + (double?)parameter;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return (double?)value - (double?)parameter;
+    }
+}
